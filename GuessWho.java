2d0b65d8f9a549485f6eb367345bf68bd9ce242b
Vasilikii <94742779+Vasilikii@users.noreply.github.com>
2021-11-23 21:07:45 +0200

    Update GuessWho.java

--- GuessWho.java
+++ GuessWho.java
@@ -10,42 +10,42 @@ public class GuessWho {
     private final int guessLimit = 10;
     private boolean outofGuesses = false;
 
+    public void printInstructions() {
+	    System.out.println("Η τελευταία δοκιμάσια απαιτεί να μαντέψετε το όνομα του μυθικού προσώπου");
+	    System.out.println("Οι κανόνες του παιχνιδιού είναι η εξής:");
+	    System.out.println("Σας δίνονται σταδιακά 10 hints. Σύμφωνα με αυτά πρέπει να βρείτε το πρόσωπο που αποκρύπτεται.");
+	    System.out.println("Έχετε τρεις προσπάθειες.");
+	    System.out.println("Καλή Επιτυχία!");
+	
+    }
+    public void startGuessing() {
+	    while(!guess.equals(secretWord)&& !outofGuesses) {
+		 if(lives<3 && /*timer>0)*/ {
+			 if(guessCount < guessLimit) {
+                                 JOptionPane.showMessageDialog(null, Questions[QuestionCount]);
+                                 String who = JOptionPane.showInputDialog(null, "Enter name");
+                                 if(who == name) {
+                                          String message = String.format("You Won!");
+                                          JOptionPane.showMessageDialog(null, message);
+                                 } else {
+                                          String message = String.format("Wrong guess");
+                                          JOptionPane.showMessageDialog(null, message);
+                                          QuestionCount++;
+                                          lives++;
+                                 }
+                         } else {
+                         outofGuesses = true;
+                         }
+                         if(outofGuesses) {
+                                String message = String.format("You Lose!No more guesses");
+                                JOptionPane.showMessageDialog(null, message);
+                         } else {
+                                String message = String.format("You Won!");
+                                JOptionPane.showMessageDialog(null, message);
+                         }
 
-
-					while(!guess.equals(secretWord)&& !outofGuesses) {
-
-                            if(lives<3 && /*timer>0)*/ {
-
-        if(guessCount < guessLimit) {
-        JOptionPane.showMessageDialog(null, Questions[QuestionCount]);
-        String who = JOptionPane.showInputDialog(null, "Enter name");
-
-        if(who == name) {
-        String message = String.format("You Won!");
-        JOptionPane.showMessageDialog(null, message);
-        } else {
-        String message = String.format("Wrong guess");
-        JOptionPane.showMessageDialog(null, message);
-        QuestionCount++;
-        lives++;
-        }
-        } 	else {
-        outofGuesses = true;
-        }
-        if(outofGuesses) {
-        String message = String.format("You Lose!No more guesses");
-        JOptionPane.showMessageDialog(null, message);
-        } else {
-
-        String message = String.format("You Won!");
-        JOptionPane.showMessageDialog(null, message);
-        }
-
-        } else {
-        break;
-        }
-
-        }
-
-        }
-
+                 } else {
+                         break;
+                 }
+            }
+    }
